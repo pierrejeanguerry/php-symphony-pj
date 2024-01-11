e1641fe140c2e33357641926fb4b23960c53fed2
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2024-01-11 12:21:00 +0100

    simple logger
    log info when a /items/ page is shown, log errors when there no items found

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\Item;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,13 +12,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class ItemController extends AbstractController
 {
     #[Route('/items/{pageNumber}', name:'item-view')]
-    public function show(EntityManagerInterface $entityManager, array $_route_params): Response
+    public function show(EntityManagerInterface $entityManager, array $_route_params, LoggerInterface $logger): Response
     {
         $itemByPages = 10;
+        $logger->info('Show route /items/{pageNumber}', 
+            ['pageNumber' => $_route_params['pageNumber']]);
+
         // $items = $entityManager->getRepository(Item::class)->findAll();
-        $items = $entityManager->getRepository(Item::class)->findBy([], null, $itemByPages, $_route_params['pageNumber'] * $itemByPages);
+        $items = $entityManager->getRepository(Item::class)->findBy([], 
+                                                    null, 
+                                                    $itemByPages, 
+                                                    $_route_params['pageNumber'] * $itemByPages);
 
         if (!$items) {
+            $logger->error('No Items Found');
             throw $this->createNotFoundException(
                 'No Items found '
             );
