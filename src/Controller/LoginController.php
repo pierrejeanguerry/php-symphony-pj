018bd62ec6de0c1141e3c903c5e0eaf67483f2ee
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2024-01-12 16:13:35 +0100

    login in works

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -3,31 +3,38 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
+use App\Form\Type\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'login-page')]
-    public function new(Request $request): Response
+    #[Route('/login', name: 'login_page')]
+    public function new(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
         $user = new User();
+        $user->setUsername("");
+        $user->setRoles(["ROLE_USER"]);
         $user->setRole("rédacteur");
-        $user->setRole("rédacteur");
+        $user->setPassword("");
         
+        $error = $authenticationUtils->getLastAuthenticationError();
+
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()){
             $user = $form->getData();
-            return $this->redirectToRoute('home_page');
+            print_r($user);
+            // return $this->redirectToRoute('home_page');
         }
 
-        return $this->render('task/login.html.twig', [
+        return $this->render('login/index.html.twig', [
             'form' => $form,
+            'error' => $error
         ]);
     }
 }
