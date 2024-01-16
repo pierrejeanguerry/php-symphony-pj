cdda4a2b9d35870487807d27291e9317d6f72e58
pierrejeanguerry <pierre-jean.guerry@ecole-hexagone.com>
2024-01-16 18:24:07 +0100

    install API Plateform

--- config/bundles.php
+++ config/bundles.php
@@ -16,4 +16,6 @@ return [
     Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
